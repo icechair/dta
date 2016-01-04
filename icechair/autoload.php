1f0e563c3a9b3a9208d1ab27554c6d1373a80ccf
icechair <ic3ch4ir@gmail.com>
2016-01-04 18:21:20 +0100

    dirname

--- icechair/autoload.php
+++ icechair/autoload.php
@@ -1,6 +1,5 @@
 <?php
-$__libpath = '/path/to/icechair/dta/';
-
+$__libpath = dirname(__FILE__).'/dta/';
 $files = glob($__libpath.'*.php');
 $files = array_merge($files, glob($__libpath.'Record/*.php'));
 $files = array_merge($files, glob($__libpath.'Segment/*.php'));
